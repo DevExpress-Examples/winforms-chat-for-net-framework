3748435b32be84279942b45d1dbca51783ecd001
Alexander Mikhailov <alexander.mikhailov@devexpress.com>
2025-07-25 12:20:56 +0400

    Clean Up

--- CS/DevExpress.AI.WinForms.HtmlChat/ChatControl.cs
+++ CS/DevExpress.AI.WinForms.HtmlChat/ChatControl.cs
@@ -1,5 +1,6 @@
 ﻿namespace DevExpress.AI.WinForms.HtmlChat {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Drawing;
     using System.Threading.Tasks;
@@ -14,6 +15,7 @@
     [ToolboxItem(true)]
     public partial class ChatControl : XtraUserControl {
         BindingList<ChatMessage> messages = new BindingList<ChatMessage>();
+
         public ChatControl() {
             InitializeComponent();
             if(!DesignMode) {
@@ -23,15 +25,21 @@
             }
         }
 
-        void InitializeStyles() {
-            Styles.TypingBox.Apply(typingBox);
-            Styles.NoMessages.Apply(messagesItemsView.EmptyViewHtmlTemplate);
-        }
         void InitializeBindings() {
             gridControl.DataSource = messages;
             messagesItemsView.TopRowPixelChanged += (sender, e) => {
                 messageMenuPopup.Hide();
             };
+        }
+        void InitializeMessageEdit() {
+            var autoHeightEdit = messageEdit as IAutoHeightControlEx;
+            autoHeightEdit.AutoHeightEnabled = true;
+            autoHeightEdit.HeightChanged += OnMessageHeightChanged;
+        }
+
+        void InitializeStyles() {
+            Styles.TypingBox.Apply(typingBox);
+            Styles.NoMessages.Apply(messagesItemsView.EmptyViewHtmlTemplate);
         }
 
         async void MessageEdit_PreviewKeyDown(object sender, KeyEventArgs e) {
@@ -42,25 +50,9 @@
                 await SendMessage(text);
             }
         }
-
-        async void TypingBox_ElementMouseClick(object sender, Utils.Html.DxHtmlElementMouseEventArgs e) {
-            if(e.ElementId == "btnSend") {
-                await SendMessage(messageEdit.Text);
-            }
-            if(e.ElementId == "btnRemove") {
-                messages.Clear();
-            }
-        }
-
-        async Task SendMessage(string userContent) {
-            if(string.IsNullOrEmpty(userContent))
-                return;
-            IChatClient service = AIExtensionsContainerDesktop.Default.GetService<IChatClient>();
-            messages.Add(new ChatMessage(ChatRole.User, userContent));
-            messagesItemsView.MoveLast();
-            ChatResponse chatResponse = await service.GetResponseAsync(messages);
-            messages.AddMessages(chatResponse);
-            messagesItemsView.MoveLast();
+        void OnMessageHeightChanged(object sender, EventArgs e) {
+            var contentSize = typingBox.GetContentSize();
+            typingBox.Height = contentSize.Height;
         }
 
         void OnMessagesViewElementMouseClick(object sender, ItemsViewHtmlElementMouseEventArgs e) {
@@ -68,23 +60,6 @@
                 if(!string.IsNullOrEmpty(messagesItemsView.SelectedText))
                     ShowContextMenu(e);
             }
-        }
-
-        void ShowContextMenu(ItemsViewHtmlElementMouseEventArgs e) {
-            Styles.ContextMenu.Apply(messageMenuPopup);
-            var size = ScaleDPI.ScaleSize(new Size(212, 100));
-            var location = new Point(e.X - size.Width / 2, e.Y - size.Height + ScaleDPI.ScaleVertical(8));
-            Rectangle screenRect = gridControl.RectangleToScreen(new Rectangle(location, size));
-            messageMenuPopup.Show(gridControl, screenRect);
-        }
-        void InitializeMessageEdit() {
-            var autoHeightEdit = messageEdit as IAutoHeightControlEx;
-            autoHeightEdit.AutoHeightEnabled = true;
-            autoHeightEdit.HeightChanged += OnMessageHeightChanged;
-        }
-        void OnMessageHeightChanged(object sender, EventArgs e) {
-            var contentSize = typingBox.GetContentSize();
-            typingBox.Height = contentSize.Height;
         }
         void OnQueryItemTemplate(object sender, QueryItemTemplateEventArgs e) {
             var message = e.Row as ChatMessage;
@@ -98,12 +73,69 @@
             string htmlString = md.Transform(message.Text);
             e.Template.Template = e.Template.Template.Replace("${Text}", htmlString);
         }
+
+        void ShowContextMenu(ItemsViewHtmlElementMouseEventArgs e) {
+            Styles.ContextMenu.Apply(messageMenuPopup);
+            var size = ScaleDPI.ScaleSize(new Size(212, 100));
+            var location = new Point(e.X - size.Width / 2, e.Y - size.Height + ScaleDPI.ScaleVertical(8));
+            Rectangle screenRect = gridControl.RectangleToScreen(new Rectangle(location, size));
+            messageMenuPopup.Show(gridControl, screenRect);
+        }
+
+        async void TypingBox_ElementMouseClick(object sender, Utils.Html.DxHtmlElementMouseEventArgs e) {
+            if(e.ElementId == "btnSend") {
+                await SendMessage(messageEdit.Text);
+            }
+            if(e.ElementId == "btnRemove") {
+                ClearMessages();
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e) {
+            base.OnHandleCreated(e);
+            if(!DesignMode) {
+                IChatClient service = AIExtensionsContainerDesktop.Default.GetService<IChatClient>();
+                if(service == null) {
+                    throw new InvalidOperationException("IChatClient service is not registered in AIExtensionsContainerDesktop.");
+                }
+            }
+        }
+
+        public void SetMessages(IEnumerable<ChatMessage> chatMessages) {
+            if(chatMessages == null)
+                throw new ArgumentNullException(nameof(chatMessages));
+            messages.Clear();
+            foreach(var message in chatMessages) {
+                if(message == null)
+                    continue;
+                messages.Add(message);
+            }
+            if(messages.Count > 0)
+                messagesItemsView.MoveLast();
+        }
+
+        public void ClearMessages() {
+            messages.Clear();
+        }
+
+        public async Task SendMessage(string userContent) {
+            if(string.IsNullOrEmpty(userContent))
+                return;
+            IChatClient service = AIExtensionsContainerDesktop.Default.GetService<IChatClient>();
+            messages.Add(new ChatMessage(ChatRole.User, userContent));
+            messagesItemsView.MoveLast();
+            ChatResponse chatResponse = await service.GetResponseAsync(messages);
+            messages.AddMessages(chatResponse);
+            messagesItemsView.MoveLast();
+        }
+
         sealed class Styles {
+            public static Style ContextMenu = new ContextMenuStyle();
             public static Style Message = new MessageStyle();
             public static Style MyMessage = new MyMessageStyle();
             public static Style NoMessages = new NoMessagesStyle();
-            public static Style ContextMenu = new ContextMenuStyle();
             public static Style TypingBox = new TypingBoxStyle();
+
             //
             sealed class MessageStyle : Style { }
             sealed class MyMessageStyle : Style { }
